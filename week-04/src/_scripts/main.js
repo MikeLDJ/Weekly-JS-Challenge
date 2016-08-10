050be2621763f990af5089e0243ff8a99e67c7d5
MikeLDJ <mikeu76@gmail.com>
2016-08-11 01:38:19 +0200

    connect to giphy

--- week-04/src/_scripts/main.js
+++ week-04/src/_scripts/main.js
@@ -1,13 +1,35 @@
 var window = document.querySelector('form');
-
+var searchText = '';
+var publicKey = 'dc6zaTOxFJmzC';
+var error = '';
 window.addEventListener('keyup', app);
 
 function app() {
-  console.log('app is working');
+  getSearchText();
+  console.log(searchText);
+  setTimeout(searchGiphy(), 3000);
+
 }
 
 function isJavaScriptEnabled() {
   document.getElementById('searchState').textContent = 'waiting for input...';
+}
+
+function getSearchText() {
+  searchText = document.getElementById('searchField').value;
+}
+
+function searchGiphy() {
+  fetch('http://api.giphy.com/v1/gifs/search?q=' + searchText + '&api_key=' + publicKey)
+  .then(function(response) {
+    console.log(response);
+    return response;
+  })
+  .then(function() {
+    console.log('response for ' + searchText);
+    document.getElementById('searchState').textContent = 'ok';
+  })
+  .catch(error);
 }
 
 isJavaScriptEnabled();
