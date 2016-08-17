dd70d86e31f028e7f55bc5d476cb41861d54ea51
MikeLDJ <mikeu76@gmail.com>
2016-08-17 03:32:57 +0200

    get json from giphy

--- week-04/src/_scripts/main.js
+++ week-04/src/_scripts/main.js
@@ -2,17 +2,33 @@ var window = document.querySelector('form');
 var searchText = '';
 var publicKey = 'dc6zaTOxFJmzC';
 var error = '';
-window.addEventListener('keyup', app);
+
+window.addEventListener('keyup', debounce(app, 500));
+
+function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+}
 
 function app() {
   getSearchText();
   console.log(searchText);
-  setTimeout(searchGiphy(), 3000);
+  searchGiphy();
 
 }
 
 function isJavaScriptEnabled() {
-  document.getElementById('searchState').textContent = 'waiting for input...';
+  document.getElementById('searchState').textContent = 'waiting for input....';
 }
 
 function getSearchText() {
@@ -22,14 +38,17 @@ function getSearchText() {
 function searchGiphy() {
   fetch('http://api.giphy.com/v1/gifs/search?q=' + searchText + '&api_key=' + publicKey)
   .then(function(response) {
-    console.log(response);
-    return response;
+    return response.json();
   })
-  .then(function() {
-    console.log('response for ' + searchText);
-    document.getElementById('searchState').textContent = 'ok';
+  .then(function(o) {
+    console.log(o.data.length);
+    console.log(o.data[0]);
+    document.getElementById('searchState').textContent = o.data[0];
   })
-  .catch(error);
+  .catch(function(error){
+    document.getElementById('searchState').textContent = "error";
+  });
 }
+
 
 isJavaScriptEnabled();
